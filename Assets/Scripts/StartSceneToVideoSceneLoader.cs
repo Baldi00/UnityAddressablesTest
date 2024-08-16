9a0dacd25e0f16ddae19f9a917ef689f04c18459
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2024-08-16 17:35:09 +0200

    Added clear cache

--- Assets/Scripts/StartSceneToVideoSceneLoader.cs
+++ Assets/Scripts/StartSceneToVideoSceneLoader.cs
@@ -5,6 +5,11 @@ public class StartSceneToVideoSceneLoader : MonoBehaviour
     [SerializeField]
     private AssetReferenceSceneAsset sceneToLoad;
 
+    private void Start()
+    {
+        Caching.ClearCache();
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.V))
